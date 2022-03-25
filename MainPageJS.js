ec0cbfb16480bcbaaa3e7a15f0a89882c7f242a4
Nick-Tran1501 <68211831+Nick-Tran1501@users.noreply.github.com>
2022-03-25 23:25:41 +0700

    Function search

--- MainPageJS.js
+++ MainPageJS.js
@@ -1,4 +1,20 @@
 function SearchBar(){
-    var searchbarinput = document.getElementById('searchbar');
-    
-}
+    var searchbarinput = document.getElementById("searchbar");
+    var filter = searchbarinput.value.toUpperCase();
+    var ul= document.getElementById("myUL");
+    var li = ul.getElementsByTagName("li");
+    var i;
+    var a;
+    var Inputvalue;
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        Inputvalue = a.textContent || a.innerText;
+        if (Inputvalue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } 
+        else {
+            li[i].style.display = "none";
+        }
+
+    }
+};
