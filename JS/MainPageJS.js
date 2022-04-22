f04173d1dbd1f1e919cf614df49f90770da57fbb
Nick-Tran1501 <68211831+Nick-Tran1501@users.noreply.github.com>
2022-04-22 22:10:14 +0700

    update

--- JS/MainPageJS.js
+++ JS/MainPageJS.js
@@ -19,35 +19,7 @@ document.getElementById("search-button").onclick =function(){
     location.replace("displayPage.html")
 };
 
-// Overlay dark
 
-// if(buildingNum != ""){
-//    
-// }
-// else{
-//     location.reload();
-// }
-// Initialize Firebase and Firebase-services
-
-/* Searchbar filter function */
-// function SearchBar(){
-//     var searchbarinput = document.getElementById("searchbar");
-//     var filter = searchbarinput.value.toUpperCase();
-//     var ul= document.getElementById("myUL");
-//     var li = ul.getElementsByTagName("li");
-//     var i;
-//     var a;
-//     var Inputvalue;
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("a")[0];
-//         Inputvalue = a.textContent || a.innerText;
-//         if (Inputvalue.toUpperCase().indexOf(filter)>-1) {
-//             li[i].style.display="";
-//             li[i].style.visibility = "visible";
-//         }
-//         else {
-//             li[i].style.display="none";
-//             li[i].style.visibility = "hidden";
-//         };
-//     };
-// };
+//check not null, bắt đầu bằng "B","b", 8-9 kí tự
+// mỗi trường hợp sai sẽ đưa ra lỗi đó 
+// 
