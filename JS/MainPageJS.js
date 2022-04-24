231d485c8aac1533e48e61d97a6aeb39f7ce7779
Nick-Tran1501 <68211831+Nick-Tran1501@users.noreply.github.com>
2022-04-24 16:04:32 +0700

    Update

--- JS/MainPageJS.js
+++ JS/MainPageJS.js
@@ -13,8 +13,22 @@
 // Get user input
 localStorage.clear();
 
-document.getElementById("search-button").onclick =function(){
+// document.getElementById("search-button").onclick =function(){
+//     var input = document.getElementById("search-input").value;
+    
+//     if(input != ""){
+//         var data = input.toUpperCase();
+//         localStorage.setItem("userInput",data);
+//         location.replace("displayPage.html")
+//     }
+//     else{
+//         alert("Please input Room Number or School service")
+//         location.reload();
+//     }
+// };
+function checkdone(){
     var input = document.getElementById("search-input").value;
+    
     if(input != ""){
         var data = input.toUpperCase();
         localStorage.setItem("userInput",data);
@@ -24,9 +38,48 @@ document.getElementById("search-button").onclick =function(){
         alert("Please input Room Number or School service")
         location.reload();
     }
-};
+}
+document.getElementById("search-button").onclick =function(){
+
+    var elem = document.getElementById("search-input").value;
+    var re = /^B+[0-10]+\.+[0-9][0-9]+\.+[0-9][0-9]$/;
+
+    if(re.test(elem)){
+       checkdone();
+     
+    }
+    else{
+        alert("Your Input is Wrong. Example:B1.01.01")
+        location.reload();
+       
+    }
+  
+}
 
 
 //check not null, bắt đầu bằng "B","b", 8-9 kí tự
 // mỗi trường hợp sai sẽ đưa ra lỗi đó 
-// 
+// document.getElementById("search-button").onclick =function(){
+// var input = document.getElementById("search-input").value;
+// var pattern = elem.getAttribute("B+[0-9]+\.+[0-9]+\.+[0-9]+{8}");
+//         var re = new RegExp(pattern);
+// if(re.test(input)){
+
+//     localStorage.clear();
+//     var input = document.getElementById("search-input").value;
+//     if(input != ""){
+//         var data = input.toUpperCase();
+//         localStorage.setItem("userInput",data);
+//         location.replace("displayPage.html");
+//     }
+//     else{
+//         alert("Please input Room Number or School service");
+//         location.reload();
+//     };
+
+// }
+// else{
+//     alert("Wrong");
+// }
+// };
+
